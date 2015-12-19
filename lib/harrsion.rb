a88c806205d1524ed7837a18635f72bee28dde1c
Roman C. Podolski <roman.podolski89@gmail.de>
2015-12-19 16:41:59 +0100

    Adds a parser for expressions
    Moves all code to the module harrsion, since parser conflicts with some
    new classes in ruby version 2.2.3. Implements a parser for expression
    statements.

--- lib/harrsion.rb
+++ lib/harrsion.rb
@@ -1,6 +1,15 @@
-require "harrsion/version"
-require "parser"
-
-module Harrsion
-  include Parser
-end
+require 'harrsion/version'
+require 'harrsion/expression'
+require 'harrsion/lexer'
+require 'harrsion/and'
+require 'harrsion/atom'
+require 'harrsion/exists'
+require 'harrsion/false'
+require 'harrsion/forall'
+require 'harrsion/formula'
+require 'harrsion/iff'
+require 'harrsion/imp'
+require 'harrsion/not'
+require 'harrsion/or'
+require 'harrsion/true'
+require 'harrsion/parser'
